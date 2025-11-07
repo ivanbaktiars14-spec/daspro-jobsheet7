144467f8e060969df837b4978a9c32d08d5ca693
ivanbaktiars14-spec <ivanbaktiars14@gmail.com>
2025-11-07 10:47:16 +0700

    perulangan14modifikasi

--- perulangan14.java
+++ perulangan14.java
@@ -2,22 +2,30 @@ import java.util.Scanner;
 
 public class perulangan14 {
     public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-         //inisialisasi tertinggi//
-         double nilai, tertinggi = 0, terendah = 100;
-         for ( int i = 1; i <= 10; i++) {
-            System.out.print(" masukkan nilai mahsiswa ke-" + i + ": ");
+        Scanner sc = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
-            if (nilai > tertinggi ) {
-                tertinggi = nilai ;
-            }
-            if (nilai < terendah) {
-                terendah = nilai ;
-            }
-            
+
+            if (nilai > tertinggi)
+                tertinggi = nilai;
+
+            if (nilai < terendah)
+                terendah = nilai;
+
+            if (nilai >= 60)
+                lulus++;
+            else
+                tidakLulus++;
+        }
+
+        System.out.println("\nNilai tertinggi = " + tertinggi);
+        System.out.println("Nilai terendah = " + terendah);
+        System.out.println("Jumlah mahasiswa lulus = " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus = " + tidakLulus);
     }
-            // nilai tertinggi dan terendah //
-            System.out.println(" nilai tertinggi" + tertinggi);
-            System.out.println(" nilai terendah"+ terendah );
-}
 }
